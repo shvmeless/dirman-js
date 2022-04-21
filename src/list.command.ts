d1def2e0c6545fd380e58b1cf4cee2f7a2e445f3
Luciano Aedo Puga <shvme.development@gmail.com>
2022-04-21 09:24:11 -0400

    feat: add a commnad to list directories and files

---
+++ src/list.command.ts
@@ -0,0 +1,44 @@
+import { Argument, Option, program } from 'commander'
+import DirManager from './classes/DirManager'
+import { resolve } from 'path'
+
+// ARGUMENTS
+const args = { path: new Argument( '[path]', 'Set the directory to search.' ).default( '.', 'Current location.' ) }
+
+// OPTIONS
+const options = {
+	creation: new Option( '-c, --creation', 'Show the creation date.' ),
+	update: new Option( '-m, --update', 'Show the modification date.' ),
+	size: new Option( '-s, --size', 'Show the directory size.' ),
+}
+
+// PROGRAM
+program
+	.description( 'Lists the directories and files found in the specified location.' )
+	.addArgument( args.path )
+	.addOption( options.creation )
+	.addOption( options.update )
+	.addOption( options.size )
+	.action( ( path, options ) => {
+
+		const { creation, update, size } = options
+
+		path = resolve( process.cwd(), path )
+
+		const dirManager = DirManager.create( path, creation, update, size )
+
+		if ( !dirManager ) {
+			console.log( 'ERROR: No such file or directory!' )
+			process.exit( 1 )
+		}
+
+		dirManager.getDirectories().forEach( ( dir ) => {
+			process.stdout.write( `${ dir.name }\n` )
+		} )
+
+		process.exit( 0 )
+
+	} )
+
+// RUN
+program.parse()
